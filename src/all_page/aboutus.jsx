f4600f992b024ab63ff9022393021354b266f7a3
Abhay vishwakarma <gaamagaming@gmail.com>
2024-03-02 20:57:11 +0530

    updating about us

--- src/all_page/aboutus.jsx
+++ src/all_page/aboutus.jsx
@@ -1,16 +1,77 @@
-import React from 'react'
-import Nav from '../componaints/nav'
-import Footer from '../componaints/footer'
-import Commingsoon from '../componaints/commingsoon'
+import React from "react";
+import Nav from "../componaints/nav";
+import Footer from "../componaints/footer";
+import Commingsoon from "../componaints/commingsoon";
 
 function Aboutus() {
   return (
     <div>
-     <Nav/>
-     <Commingsoon/>
-     <Footer/>
+      <Nav />
+      <div className=" w-full pt-14">
+        <h1 className=" w-full pl-5 text-5xl tracking-tighter font-semibold">
+          Welcome to our about section...
+        </h1>
+        <p className=" tracking-tight pt-7 w-[95%] opacity-80 leading-6 pl-10 flex flex-col gap-4">
+          <span>
+            Welcome to Plick Courses, your premier destination for top-notch
+            online education! At Plick, we're passionate about empowering
+            learners like you to acquire valuable skills and knowledge in the
+            rapidly evolving tech industry. Whether you're interested in delving
+            into the intricacies of web development, exploring the endless
+            possibilities of android app creation, honing your design skills
+            with UI/UX principles, or diving into other exciting fields, we've
+            got you covered.
+          </span>
+
+          <span>
+            Ourcurated courses are meticulously crafted by industry experts who
+            bring real-world experience and expertise to the virtual classroom.
+            We understand that the journey of learning is unique for each
+            individual, which is why our courses cater to learners of all
+            levels, from beginners to advanced professionals. Through
+            interactive lessons, hands-on projects, and engaging assessments, we
+            strive to make your learning experience both enjoyable and
+            effective.
+          </span>
+
+          <span>
+            At Plick,accessibility is a core value. That's why our courses are
+            designed to be flexible and convenient, allowing you to learn at
+            your own pace, anytime and anywhere. Whether you're a full-time
+            student, a working professional, or simply someone with a curious
+            mind, Plick Courses offers the flexibility you need to pursue your
+            educational goals. Join our vibrant community of learners and
+            discover the endless possibilities that await you with Plick
+            Courses. Embark on a journey of personal and professional growth,
+            and let us help you turn your aspirations into achievements. Start
+            your learning journey today with Plick Courses!
+          </span>
+        </p>
+        <h1 className=" w-full pl-5 pt-10 text-4xl tracking-tighter font-semibold">
+          About web-development...
+        </h1>
+        <p className=" tracking-tight pt-7 w-[95%] opacity-80 leading-6 pl-10 flex flex-col gap-4 pb-10">
+          Embark on a transformative journey into the dynamic world of web
+          development with Plick Courses. Our web development curriculum is
+          designed to equip you with the essential skills and knowledge needed
+          to thrive in this ever-evolving field. From mastering the fundamentals
+          of HTML, CSS, and JavaScript to exploring advanced frameworks and
+          technologies like React, Angular, and Node.js, our courses cover the
+          full spectrum of web development. Led by industry-leading instructors,
+          our courses feature hands-on projects, real-world applications, and
+          interactive exercises to reinforce your learning and deepen your
+          understanding. Whether you're a beginner looking to break into the
+          industry or a seasoned professional seeking to stay ahead of the
+          curve, our web development courses offer something for everyone. Join
+          us and unlock the power of web development to create stunning
+          websites, dynamic web applications, and immersive online experiences.
+          Take the first step towards a rewarding career or hobby in web
+          development with Plick Courses.
+        </p>
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Aboutus
+export default Aboutus;
