9a7fbf7adb5e5f4b6469ee413a572b1e614be9d0
Abhay vishwakarma <gaamagaming@gmail.com>
2024-03-03 22:28:45 +0530

    nav updated

--- src/componaints/features.jsx
+++ src/componaints/features.jsx
@@ -10,6 +10,7 @@ useEffect(() => {
   const open_icon = document.querySelector(".open_icon");
   const close_icon = document.querySelector(".close_icon");
   const main_nav = document.querySelector(".main_nav");
+  const max630_links = document.querySelector(".max630_links");
 
   close_icon.addEventListener("click", (e) => {
     max630_hide.style.display = "block";
@@ -19,6 +20,8 @@ useEffect(() => {
     menu_bar3line.style.display = "block";
     open_icon.style.display = "block";
     main_nav.style.backgroundColor = "transparent"
+    max630_links.style.height = "100vh"
+    max630_links.style.padding = "20vh"
   })
   open_icon.addEventListener("click",(e) => {
     max630_ul.style.display = "none";
@@ -26,6 +29,8 @@ useEffect(() => {
     open_icon.style.display = "none";
     max630_bgclr.style.display = "none";
     main_nav.style.backgroundColor = "#1f1f1fe8"
+    max630_links.style.height = "0vh"
+    max630_links.style.padding = "0vh"
   })
 });
      
