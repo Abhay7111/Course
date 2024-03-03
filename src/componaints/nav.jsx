7975e10cda2bff7a03ab019217a65abef7c3328d
Abhay vishwakarma <gaamagaming@gmail.com>
2024-03-03 22:08:35 +0530

    nav updated

--- src/componaints/nav.jsx
+++ src/componaints/nav.jsx
@@ -5,7 +5,7 @@ import Features from './features';
 function Nav() {
   return (
     <div>
-    <div className=' max630_bgclr absolute top-0 left-0 w-full h-screen sm:hidden sm:z-10'></div>
+    <div className=' max630_bgclr absolute top-0 left-0 h-screen sm:hidden sm:z-10'></div>
      <div className='main_nav w-full h-14 flex items-center px-7 justify-between sm:bg-yellow-400/0 md:bg-red-400 md:px-4 lg:bg-zinc-700/0'>
           <div className="sm:logo max630_logo text-[1.8rem] font-bold w-12 break-words leading-[23px] sm:scale-[1.2] sm:mt-3">PLICK</div>
           <div className=' sm:w-[70vw] max630_hide sm:flex items-center gap-8 justify-end sm:gap-4 md:gap-4 lg:gap-8'>
