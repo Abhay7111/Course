ad96068525ef8e725fd0d88dbbaa0518e9aa651d
Abhay vishwakarma <gaamagaming@gmail.com>
2024-03-03 20:24:14 +0530

    responcive nav updated

--- src/componaints/features.jsx
+++ src/componaints/features.jsx
@@ -2,12 +2,14 @@ import React, { useEffect } from 'react'
 
 function Features() {
 
+useEffect(() => {
   const menu_bar3line = document.querySelector(".menu_bar3line");
   const max630_hide = document.querySelector(".max630_hide");
 
-  menu_bar3line.addEventListener("click", (e)=>{
-    max630_hide.style.display = "block"
+  menu_bar3line.addEventListener("click", (e) => {
+    max630_hide.style.display = "block";
   })
+});
      
   return (
     <div>Features</div>
