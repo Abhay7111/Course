daec547ad5df5e81432de069db718eb93f993bcf
Abhay vishwakarma <142080096+Abhay7111@users.noreply.github.com>
2024-07-16 21:26:18 +0530

    fgbb

--- src/main.jsx
+++ src/main.jsx
@@ -139,6 +139,5 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-    {/* <Preloader/> */}
   </React.StrictMode>
 );
